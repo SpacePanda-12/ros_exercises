851f5d1dcf1789ea6c87f68c80dd511a2dd8fbf1
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-11 18:54:42 -0500

    small change to make node not anonymous

--- scripts/simple_publisher.py
+++ scripts/simple_publisher.py
@@ -9,7 +9,7 @@ import random
 
 def simple_publisher():
     pub = rospy.Publisher('my_random_float', Float32, queue_size=10)
-    rospy.init_node('simple_publisher', anonymous=True)
+    rospy.init_node('simple_publisher')
     rate = rospy.Rate(20)
 
     while not rospy.is_shutdown():

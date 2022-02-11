c172b01c25797ad30593b6457eb314492d96a88f
SpacePanda-12 <dfgoff22@gmail.com>
2022-02-11 23:12:40 +0000

    restructure

---
+++ scripts/simple_publisher.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+# from geometry_msgs.msg import Twist, Point
+# from sensor_msgs.msg import LaserScan
+import random
+
+
+def simple_publisher():
+    pub = rospy.Publisher('my_random_float', Float32, queue_size=10)
+    rospy.init_node('simple_publisher', anonymous=True)
+    rate = rospy.Rate(20)
+
+    while not rospy.is_shutdown():
+        number = random.uniform(0, 10)
+        pub.publish(number)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        simple_publisher()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+
+
+
+
+
+
+

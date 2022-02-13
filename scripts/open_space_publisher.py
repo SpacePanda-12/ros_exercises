67f58fc87a9d5b46e62f24e98445beeff324c9bf
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-12 22:12:29 -0500

    Add files via upload

--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -5,21 +5,27 @@ import math
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 from OpenSpace.msg import distance
-from OpenSpace.msg import angle
+from ros_exercises.msg import OpenSpace
 global rate
 
-dist_pub = rospy.Publisher('open_space', distance, queue_size=10)
-angle_pub = rospy.Publisher('open_space', angle, queue_size=10)
+# dist_pub = rospy.Publisher('open_space', distance, queue_size=10)
+# angle_pub = rospy.Publisher('open_space', angle, queue_size=10)
+pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
 
 
 
 def callback(data):
     index = data.ranges.index(max(data.ranges), 0, len(data.ranges))
     angle = float(-2)/3*np.pi + float(index) * float(1)/300*np.pi
-    dist_pub.publish(max(data.ranges))
-    angle_pub.publish(angle)
+    msg = OpenSpace()
+    msg.angle = angle
+    msg.distance = max(data.ranges)
+    pub.publish(msg)
+    # dist_pub.publish(max(data.ranges))
+    # angle_pub.publish(angle)
     # rospy.loginfo(max(data.ranges))
     # rospy.loginfo(angle)
+
     rate.sleep()
 
 

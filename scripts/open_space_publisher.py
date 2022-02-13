34e03a9fb047d87d5b7f3dd2530b6352ad257976
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-12 20:23:03 -0500

    create file

---
+++ scripts/open_space_publisher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+
+dist_pub = rospy.Publisher('open_space/distance', Float32, queue_size=10)
+angle_pub = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+rate = rospy.Rate(20)
+
+
+def callback(data):
+    rospy.loginfo(max(data.ranges))
+    # dist_pub.publish()
+    # angle_pub.publish()
+    rate.sleep()
+
+def simple_subscriber():
+    rospy.init_node('open_space_publisher')
+    rospy.Subscriber("fake_scan", LaserScan, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        simple_subscriber()
+    except rospy.ROSInterruptException:
+        pass

35914b441fe92f2acdecfd4fc53976a33d01e7e5
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-14 11:51:04 -0500

    Add files via upload

--- scripts/static_tf_cam_publisher.py
+++ scripts/static_tf_cam_publisher.py
@@ -5,63 +5,64 @@ import tf2_ros
 import tf
 import geometry_msgs.msg
 
-# def static_publish():
-rospy.init_node("tf2_static_broadcast")
-br = tf2_ros.StaticTransformBroadcaster()
+def static_publish():
+    rospy.init_node("tf2_static_broadcast")
+    br = tf2_ros.StaticTransformBroadcaster()
 
-# precomputed transforms for the cameras
-transform_matrix_left_from_robot = np.array([[float(1), float(0), float(0), float(-0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
-transform_matrix_right_from_robot = np.array([[float(1), float(0), float(0), float(0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
+    # precomputed transforms for the cameras
+    transform_matrix_left_from_robot = np.array([[float(1), float(0), float(0), float(-0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
+    transform_matrix_right_from_robot = np.array([[float(1), float(0), float(0), float(0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
 
-transform_matrix_left = np.array([[float(1), float(0), float(0), float(0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
-transform_matrix_right = np.array([[float(1), float(0), float(0), float(-0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
+    transform_matrix_left = np.array([[float(1), float(0), float(0), float(0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
+    transform_matrix_right = np.array([[float(1), float(0), float(0), float(-0.5)], [float(0), float(1), float(0), float(0)], [float(0), float(0), float(1), float(0)], [float(0), float(0), float(0), float(1)]])
 
-# left cam trans plus rot
-robot_to_left_quaternion = tf.transformations.quaternion_from_matrix(transform_matrix_left)
-robot_to_left_translation = np.array([transform_matrix_left[0][3], transform_matrix_left[1][3], transform_matrix_left[2][3]])
+    # left cam trans plus rot
+    robot_to_left_quaternion = tf.transformations.quaternion_from_matrix(transform_matrix_left)
+    robot_to_left_translation = np.array([transform_matrix_left[0][3], transform_matrix_left[1][3], transform_matrix_left[2][3]])
 
-robot_to_left = geometry_msgs.msg.TransformStamped()
-robot_to_left.header.stamp = rospy.Time.now()
-robot_to_left.header.frame_id = "base_link_gt"
-robot_to_left.child_frame_id = "left_cam"
+    robot_to_left = geometry_msgs.msg.TransformStamped()
+    robot_to_left.header.stamp = rospy.Time.now()
+    robot_to_left.header.frame_id = "base_link_gt"
+    robot_to_left.child_frame_id = "left_cam"
 
-# left cam translations
-robot_to_left.transform.translation.x = robot_to_left_translation[0]
-robot_to_left.transform.translation.y = robot_to_left_translation[1]
-robot_to_left.transform.translation.z = robot_to_left_translation[2]
+    # left cam translations
+    robot_to_left.transform.translation.x = robot_to_left_translation[0]
+    robot_to_left.transform.translation.y = robot_to_left_translation[1]
+    robot_to_left.transform.translation.z = robot_to_left_translation[2]
 
-# left cam rotations
-robot_to_left.transform.rotation.x = robot_to_left_quaternion[0]
-robot_to_left.transform.rotation.y = robot_to_left_quaternion[1]
-robot_to_left.transform.rotation.z = robot_to_left_quaternion[2]
-robot_to_left.transform.rotation.w = robot_to_left_quaternion[3]
+    # left cam rotations
+    robot_to_left.transform.rotation.x = robot_to_left_quaternion[0]
+    robot_to_left.transform.rotation.y = robot_to_left_quaternion[1]
+    robot_to_left.transform.rotation.z = robot_to_left_quaternion[2]
+    robot_to_left.transform.rotation.w = robot_to_left_quaternion[3]
 
-# right cam trans plus rot
-robot_to_right_quaternion = tf.transformations.quaternion_from_matrix(transform_matrix_right)
-robot_to_right_translation = np.array([transform_matrix_right[0][3], transform_matrix_right[1][3], transform_matrix_right[2][3]])
+    # right cam trans plus rot
+    robot_to_right_quaternion = tf.transformations.quaternion_from_matrix(transform_matrix_right)
+    robot_to_right_translation = np.array([transform_matrix_right[0][3], transform_matrix_right[1][3], transform_matrix_right[2][3]])
 
-robot_to_right = geometry_msgs.msg.TransformStamped()
-robot_to_right.header.stamp = rospy.Time.now()
-robot_to_right.header.frame_id = "base_link_gt"
-robot_to_right.child_frame_id = "right_cam"
+    robot_to_right = geometry_msgs.msg.TransformStamped()
+    robot_to_right.header.stamp = rospy.Time.now()
+    robot_to_right.header.frame_id = "base_link_gt"
+    robot_to_right.child_frame_id = "right_cam"
 
-# right cam translations
-robot_to_right.transform.translation.x = robot_to_right_translation[0]
-robot_to_right.transform.translation.y = robot_to_right_translation[1]
-robot_to_right.transform.translation.z = robot_to_right_translation[2]
+    # right cam translations
+    robot_to_right.transform.translation.x = robot_to_right_translation[0]
+    robot_to_right.transform.translation.y = robot_to_right_translation[1]
+    robot_to_right.transform.translation.z = robot_to_right_translation[2]
 
-# right cam rotations
-robot_to_right.transform.rotation.x = robot_to_right_quaternion[0]
-robot_to_right.transform.rotation.y = robot_to_right_quaternion[1]
-robot_to_right.transform.rotation.z = robot_to_right_quaternion[2]
-robot_to_right.transform.rotation.w = robot_to_right_quaternion[3]
+    # right cam rotations
+    robot_to_right.transform.rotation.x = robot_to_right_quaternion[0]
+    robot_to_right.transform.rotation.y = robot_to_right_quaternion[1]
+    robot_to_right.transform.rotation.z = robot_to_right_quaternion[2]
+    robot_to_right.transform.rotation.w = robot_to_right_quaternion[3]
 
-br.sendTransform([robot_to_right, robot_to_left])
-# br.sendTransform(robot_to_left)
-# rospy.spin()
+    br.sendTransform([robot_to_right, robot_to_left])
+    # br.sendTransform(robot_to_left)
+    rospy.spin()
 
-# if __name__ == '__main__':
-#     try:
-#         static_publish()
-#     except rospy.ROSInterruptException:
-#         pass
+if __name__ == '__main__':
+    try:
+        static_publish()
+        pass
+    except rospy.ROSInterruptException:
+        pass

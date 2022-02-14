e1019899de7f0835a190209602eb3b9e5fde9d0d
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2022-02-13 23:11:43 -0500

    Add files via upload

--- scripts/static_tf_cam_publisher.py
+++ scripts/static_tf_cam_publisher.py
@@ -58,5 +58,8 @@ robot_to_right.transform.rotation.y = robot_to_right_quaternion[1]
 robot_to_right.transform.rotation.z = robot_to_right_quaternion[2]
 robot_to_right.transform.rotation.w = robot_to_right_quaternion[3]
 
-br.sendTransform(robot_to_left)
-br.sendTransform(robot_to_right)
+while not rospy.is_shutdown()
+    br.sendTransform(robot_to_left)
+    br.sendTransform(robot_to_right)
+
+    r.sleep()
